80923944d626d11038ac50228e785ab7ffe862e1
Hudan Studiawan <studiawan@gmail.com>
2020-02-15 04:01:48 +0800

    Edit the sampling

--- imbalance_anomaly/methods/keras_methods.py
+++ imbalance_anomaly/methods/keras_methods.py
@@ -1,9 +1,10 @@
 import keras_metrics
+import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
 from keras.models import Sequential
 from keras.layers import Embedding, SpatialDropout1D, LSTM, Dense, Dropout, Conv1D, GlobalMaxPooling1D
-from imblearn.keras import balanced_batch_generator
+from keras.utils import to_categorical
 from imbalance_anomaly.imbalance.sampling import Sampling
 
 
@@ -26,6 +27,7 @@ class KerasModel(object):
         self.MAX_PAD = 8
         self.GLOVE_DIM = 50
         self.sampling = Sampling(sampler_name)
+        self.sampler = self.sampling.get_sampler()
         self.filters = 64
         self.kernel_size = 5
 
@@ -51,21 +53,21 @@ class KerasModel(object):
         model.compile(loss='categorical_crossentropy', optimizer='adam',
                       metrics=[keras_metrics.precision(), keras_metrics.recall(), keras_metrics.f1_score(), 'acc'])
 
-        print('SAMPLER', self.sampler_name)
         if self.sampler_name != '':
-            # imbalance sampling
-            training_generator, steps_per_epoch = \
-                balanced_batch_generator(self.x_train, self.y_train, sampler=self.sampling.get_sampler(),
-                                         batch_size=self.batch_size, random_state=self.random_seed)
+            # sample the data
+            train_resample, train_label_resample = self.sampler.fit_resample(self.x_train, self.y_train)
+            train_resample = np.asarray(train_resample)
+            train_label_resample = to_categorical(train_label_resample)
 
             # training
-            model.fit_generator(generator=training_generator, steps_per_epoch=steps_per_epoch, epochs=self.epochs)
+            model.fit(train_resample, train_label_resample, batch_size=self.batch_size, epochs=self.epochs)
 
         else:
-            model.fit(self.x_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs)
+            x_train = np.asarray(self.x_train)
+            y_train = to_categorical(self.y_train)
+            model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.epochs)
 
         self.model = model
-
         print(model.summary())
 
     def train_cnn(self):
@@ -88,16 +90,18 @@ class KerasModel(object):
                       metrics=['accuracy'])
 
         if self.sampler_name != '':
-            # imbalance sampling
-            training_generator, steps_per_epoch = \
-                balanced_batch_generator(self.x_train, self.y_train, sampler=self.sampling.get_sampler(),
-                                         batch_size=self.batch_size, random_state=self.random_seed)
+            # sample the data
+            train_resample, train_label_resample = self.sampler.fit_resample(self.x_train, self.y_train)
+            train_resample = np.asarray(train_resample)
+            train_label_resample = to_categorical(train_label_resample)
 
             # training
-            model.fit_generator(generator=training_generator, steps_per_epoch=steps_per_epoch, epochs=self.epochs)
+            model.fit(train_resample, train_label_resample, batch_size=self.batch_size, epochs=self.epochs)
 
         else:
-            model.fit(self.x_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs)
+            x_train = np.asarray(self.x_train)
+            y_train = to_categorical(self.y_train)
+            model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.epochs)
 
         self.model = model
         print(model.summary())
